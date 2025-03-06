979b9c18fd8eb3c4710194d4b18fd5f66492b98a
Namitha1605 <164128255+Namitha1605@users.noreply.github.com>
2025-03-07 00:28:34 +0530

    Update Lab3_1.py

--- lab3/Lab3_1.py
+++ lab3/Lab3_1.py
@@ -1,58 +1,41 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from pandas.plotting import autocorrelation_plot
 
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import r2_score
-from sklearn.datasets import fetch_california_housing
-from sklearn.svm import SVR
-from sklearn.ensemble import RandomForestClassifier
+# EDA
+data = pd.read_csv("simulated_data_multiple_linear_regression_for_ML.csv")
+print(data.columns)
 
-#"age","BMI","BP","blood_sugar","Gender","disease_score","disease_score_fluct"
-def load_data():
-    data = pd.read_csv("data_ML.csv")
-    x = data.drop(columns=["disease_score","disease_score_fluct"])
-    y = data["disease_score_fluct"]
-    print(y)
-    print(x)
-    return x,y
+X = data[['age', 'BMI', 'BP', 'blood_sugar', 'Gender']].values
+print(X)
+Y = data[['disease_score']].values
+print(Y)
+X = np.c_[np.ones(X.shape[0]),X]
+theta = np.zeros(X.shape[1])
+# compute theta
+def compute_cost(theta,X,Y):
+    h_theta = X @ theta
+    print(h_theta)
+    error = h_theta - Y.flatten()
+    cost = np.sum(error**2) / 2
+    return cost
+def compute_gradient(theta,X,Y,alpha=0.0001,iteration=1000):
+    cost_history = []
+    for i in range(iteration):
+        h_theta = X @ theta
+        error2= h_theta-Y.flatten()
+        theta -= alpha * ( X.T @ error2)
 
-def main():
-    # load california housing data
-    [x, y] = load_data()
+        cost = compute_cost(theta,X,Y)
+        cost_history.append(cost)
+        if i % 1 ==0:
+            print(f"the iteration:{i} and cost:{cost:.4f}")
 
-    # split data - train = 70%, test = 30%
-    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=999)
-
-    # scale the data
-    scaler = StandardScaler()
-    scaler = scaler.fit(X_train)
-    X_train_scaled = scaler.transform(X_train)
-    X_test_scaled = scaler.transform(X_test)
-
-    # train a model
-    print("-----TRAINING----")
-    # training a linear regression
-    model = LinearRegression()
-    # train the model
-    model.fit(X_train_scaled, y_train)
-
-    # prediction on a test set
-    y_pred = model.predict(X_test_scaled)
-
-    # compute the r2 score (r2 score closer to 1 is considered to be good)
-    r2 = r2_score(y_test, y_pred)
-    plt.scatter(y_test,y_pred)
-    plt.show()
-
-    print("r2 score is %0.2f" % r2)
-    print("Successfully done!")
-
-
-if __name__ == "__main__":
-    main()
-
-
-
-
+    return theta,cost_history
+theta_final,cost_hist = compute_gradient(theta,X,Y,alpha=0.000000001,iteration=1000)
+# plot that represent the convergence
+plt.plot(range(len(cost_hist)),cost_hist)
+plt.xlabel("iteration")
+plt.ylabel("cost_hist")
+plt.show()
